1acdab7b8ad4fb44cfde55ec99463f5993cd3120
Derkaziam <derkaziam@gmail.com>
2023-08-11 21:35:24 -0400

    Initial commit

---
+++ src/Constants.cs
@@ -0,0 +1,11 @@
+using Raylib_cs;
+using System.Numerics;
+
+namespace Platformer;
+public class Constants {
+    public const int screenWidth = 1280;
+    public const int screenHeight = 720;
+
+    public const int centerX = screenWidth / 2;
+    public const int centerY = screenHeight / 2;
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,31 +1,23 @@
 ﻿using Raylib_cs;
 using System.Numerics;
+using Platformer;
 
-namespace PoliticsGame;
+namespace Platformer;
 static class Program {
     public static void Main() {
 
         // Variables
-        const int screenWidth = 1280;
-        const int screenHeight = 720;
-
-        const float centerX = screenWidth / 2;
-        const float centerY = screenHeight / 2;
-
         Vector2 position = new Vector2(Raylib.GetMouseX(), Raylib.GetMouseY());
         Vector2 size = new Vector2(40, 40);
 
         // Initialize Window
-        Raylib.InitWindow(screenWidth, screenHeight, "CountyGame");
-        Raylib.SetTargetFPS(120);
+        Raylib.InitWindow(Constants.screenWidth, Constants.screenHeight, "CountyGame");
+        Raylib.SetTargetFPS(60);
 
         // Game Loop
         while (!Raylib.WindowShouldClose()) {
             Raylib.BeginDrawing();
-                Raylib.ClearBackground(Color.WHITE);
-                Raylib.DrawFPS(10, 10);
-                Raylib.DrawRectangleV(position, size, Color.BLACK);
-                Raylib.DrawText("Testing", centerX, centerY, 20, Color.BLACK);
+                Menu.MainMenu.CreateMenu();
             Raylib.EndDrawing();
         }
         
